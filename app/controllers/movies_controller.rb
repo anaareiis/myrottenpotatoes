6927fe065fad245f0d3f38e6aa149f96764514fd
Ana <ana@example.com>
2026-04-14 18:26:19 -0300

    feat: Edit a  movie (issue #4)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,6 +21,19 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path, notice: "Movie updated successfully."
+    else
+      render :edit
+    end
+  end
+
   private
 
   def movie_params
