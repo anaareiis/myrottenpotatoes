71a951a718ac42ac4b6a1124fb9570c02ad80acb
Ana <ana@example.com>
2026-04-14 11:27:21 -0300

    feat: View movies (issue #2)

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true, uniqueness: { case_sensitive: false, message: "already exists in the database" }
+  validates :rating, presence: true
+  validates :description, presence: true
+  validates :release_date, presence: true
 end
